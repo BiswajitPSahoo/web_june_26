ebdc1be84e4f95315edbb353d8acc8ff19e64a29
BiswajitPSahoo <biswajitprasadsahoo45@gmail.com>
2026-07-27 07:34:24 +0530

    - operators & interaction in js

---
+++ 04 javascript/lec 3-user interaction, operators/user interaction.js
@@ -0,0 +1,23 @@
+// alert("Hello Everyone!")
+// window.alert("Good Morning!")
+
+// let name = prompt("Enter Your name:-")
+// console.log(name)
+
+
+// if(name) {  
+//     console.log("Your name is", name);   
+// }else{
+//     // console.log("Re enter user name");
+//     console.log("name is", prompt("Re enter your user name"));    
+// }
+
+// let choice = confirm("Are you enjoying js classes?")
+// console.log(choice);
+
+// console.log("Hello");   //Normal
+// console.warn("Hii");    //Yellow
+// console.error("Bye");   //red
+
+
+
