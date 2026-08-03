714b48e8fd052b54b35bb59a705df0fad4781e46
BiswajitPSahoo <biswajitprasadsahoo45@gmail.com>
2026-08-04 04:49:52 +0530

    -function expression, arrow function, IIFE

---
+++ 04 javascript/lec 6-higher order functions, closure, Arrow Function/Clousure.js
@@ -0,0 +1,12 @@
+function increment() {
+    let number = 10
+    return function plusOne() {
+        console.log("Adding one");
+        number++
+        console.log("number value is", number);   
+    }
+}
+
+const nestedFun = increment()
+// console.log("///////", nestedFun);
+nestedFun()
---
+++ 04 javascript/lec 6-higher order functions, closure, Arrow Function/script.js
@@ -0,0 +1,26 @@
+// m.kj
+
+
+
+// console.log(1)
+// setTimeout(f1, 3000)
+// console.log(3)
+
+// function f1() {
+//     console.log("Hiiiii");   
+// }
+
+// const intervalid = setInterval(f1, 2000)
+// console.log("////////", intervalid);
+// const timerId = setTimeout(function() {
+//     console.log("Clearing interval");
+//     clearInterval(intervalid)
+//     clearTimeout(timerId)   
+// }, 10000)
+
+// setTimeout()
+
+//=================
+console.log(1);
+// const id = 
+
